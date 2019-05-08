9878f48401b1c68f0a9b4f7abbe8dc07922ededa
Alan Alberto <alansoulcomputing@gmail.com>
2019-05-08 14:57:09 -0500

    carga del proyecto
    Se cargo la primer version del proyecto

---
+++ conexion/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$sever='localhost';
+$user='root';
+$pass='';
+$base='tiendaUniversitaria';
+$conex = mysqli_connect($sever,$user,$pass,$base);
+if (!$conex) {
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo 'conexion vergas';
+}
+?>
